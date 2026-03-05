271a3306f94a78d201d29ce79a989485afa6bd0c
motekit0 <99771236+motekit0@users.noreply.github.com>
2026-03-05 15:53:46 +0100

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
 
-
-size_t plus_grand_bloc_allouable(size_t &max, size_t &min){
-    size_t max, min, moyenne;
-    while(min+1!=max){
+void RechercheMoins(){
+    size_t taille = 1;
+    size_t plus, moins;
+    bool quitter = false;
+    while(!quitter){
         try
         {
-            moyenne = (max+min)/2;
-            int tab = new int[moyenne];
+            char* tab = new char[taille];     //Création d'un tableau
+            delete[] tab;                   
+            plus = taille;                    //On sauvegarde l'ancienne taille
+            taille*=2;                        //On double la taille pour le prochain teste
+        }
+        catch(const std::bad_alloc& e){
+            moins = taille;                   //On sauvegarde la taille provoquant une erreur
+            quitter = true;
+        }
+    }
+}
+
+
+size_t plus_grand_bloc_allouable(){
+    size_t moyenne, plus, moins;
+    while(plus+1!=moins){
+        try
+        {
+            moyenne = (moins+plus)/2;
+            char* tab = new char[moyenne];
             delete[] tab;
-            min = moyenne;
+            plus = moyenne;
         }
         catch(const std::bad_alloc& e)
         {
-            max = moyenne;
+            moins = moyenne;
         }
-        
     }
-    return moyenne;
+    std::cout<<moyenne;
 }
 
+
+int main() {
+    RechercheMoins();
+    plus_grand_bloc_allouable();
+}
